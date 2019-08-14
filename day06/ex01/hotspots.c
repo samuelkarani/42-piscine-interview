f94a7e1a3bb36173cb39cc420e759f531b7f0c71
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-08-14 21:12:47 +0300

    completed 06

--- day06/ex01/hotspots.c
+++ day06/ex01/hotspots.c
@@ -6,59 +6,29 @@
 /*   By: smbaabu <smbaabu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 13:59:38 by smbaabu           #+#    #+#             */
-/*   Updated: 2019/08/12 19:40:10 by smbaabu          ###   ########.fr       */
+/*   Updated: 2019/08/14 19:43:20 by smbaabu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <string.h>
 #include "header.h"
-
-int size(struct s_hotspot **hostspots)
-{
-    int i = 0;
-    while (hostspots[i])
-        i++;
-    return i;
-}
 
 int overlap(struct s_hotspot *a, struct s_hotspot *b)
 {
     return b->pos - b->radius < a->pos + a->radius;
 }
 
-int helper(struct s_hotspot **hotspots, int n, int *visited, int i)
-{
-	int count = 1;
-	for (int j = i + 1; j < n; j++)
-	{
-		if (!visited[j] && !overlap(hotspots[i], hotspots[j]))
-		{
-			count++;
-			visited[j] = 1;
-			i = j;
-		}
-	}
-	return count;
-}
-
 int selectHotspots(struct s_hotspot **hotspots)
 {
-	int n, count, *visited, mx;
+	int idx, n;
 
-	n = size(hotspots);
-	visited = malloc(n * sizeof(int));
-	memset(visited, 0, n * sizeof(int));
-	mx = 0;
-	for (int i = 0; i < n; i++)
+	idx = 0, n = 1;
+	for (int i = 1; hotspots[i]; i++)
 	{
-		if (!visited[i] && !overlap(hotspots[0], hotspots[i]))
+		if (!overlap(hotspots[idx], hotspots[i]))
 		{
-			count = 1 + helper(hotspots, n, visited, i);
-			visited[i] = 1;
-			if (count > mx)
-				mx = count;
+			n++;
+			idx = i;
 		}
 	}
-	return mx;
+	return n;
 }
--- day06/ex06/printMST.c
+++ day06/ex06/printMST.c
@@ -6,7 +6,7 @@
 /*   By: smbaabu <smbaabu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 18:37:01 by smbaabu           #+#    #+#             */
-/*   Updated: 2019/08/14 16:17:21 by smbaabu          ###   ########.fr       */
+/*   Updated: 2019/08/14 20:54:51 by smbaabu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,135 +23,47 @@ int size(struct s_node **nodes)
 	return i;
 }
 
-struct s_connection
+int find(int *nodes, int id)
 {
-	struct s_node *a;
-	struct s_node *b;
-};
+	if (nodes[id] < 0)
+		return id;
+	return find(nodes, nodes[id]);
+}
 
-void fsort(struct s_edge **edges)
+void funion(int *nodes, int x, int y)
 {
-	struct s_edge *tmp;
-
-	for (int i = 0; edges[i + 1]; i++)
+	if (abs(nodes[x]) >= abs(nodes[y]))
 	{
-		for (int j = 0; edges[j + 1]; j++)
-		{
-			if (edges[j]->dist > edges[j + 1]->dist)
-			{
-				tmp = edges[j];
-				edges[j] = edges[j + 1];
-				edges[j + 1] = tmp;
-			}
-		}
+		nodes[x]--;
+		nodes[y] = x;
+	}
+	else
+	{
+		nodes[y]--;
+		nodes[x] = y;
 	}
 }
 
-int contains(struct s_connection *connections, int n, struct s_node *a, struct s_node *b)
+int cycle(int *nodes, int id1, int id2)
 {
-	for (int i = 0; i < n; i++)
-		if ((connections[i].a == a && connections[i].b == b)
-			|| (connections[i].a == b && connections[i].b == a))
-			return 1;
+	int x = find(nodes, id1);
+	int y = find(nodes, id2);
+	if (x == y)
+		return 1;
 	return 0;
-}
-
-void print(struct s_node *sNode, struct s_edge *sEdge)
-{
-	printf("%3d : %7s - %7s\n", sEdge->dist, sNode->name, sEdge->dest->name);
 }
 
 void printMST(struct s_graph *graph)
 {
-	struct s_node *node, *sNode, *front, *back;
-	struct s_edge *edge, *sEdge, *fEdge, *bEdge;
-	int n, fsmall, bsmall, small, idx, cost;
-
-	n = size(graph->nodes);
-	struct s_connection connections[n];
-	idx = 0;
-	for (int i = 1; i < n; i++)
-	{
-		if (i == 1)
-		{
-			small = INT_MAX;
-			for (int i = 0; i < n; i++)
-			{
-				node = graph->nodes[i];
-				fsort(node->edges);
-				for (int j = 0; node->edges[j]; j++)
-				{
-					edge = node->edges[j];
-					// printf("pre: %d -> %s - %s\n", edge->dist, node->name, edge->dest->name);
-					if (edge->dist < small && !contains(connections, idx, node, edge->dest))
-					{
-						sEdge = edge;
-						small = edge->dist;
-						sNode = node;
-						break ;
-					}
-				}
-			}
-			connections[idx++] = (struct s_connection){sNode, sEdge->dest};
-			front = sNode;
-			back = sEdge->dest;
-			print(sNode, sEdge);
-		}
-		else
-		{
-			fsmall = INT_MAX;
-			for (int i = 0; front->edges[i]; i++)
-			{
-				edge = front->edges[i];
-				// printf("front: %d -> %s - %s\n", edge->dist, front->name, edge->dest->name);
-				if (edge->dist < fsmall && !contains(connections, idx, front, edge->dest))
-					{
-						fEdge = edge;
-						fsmall = edge->dist;
-						break ;
-					}
-			}
-			bsmall = INT_MAX;
-			for (int i = 0; back->edges[i]; i++)
-			{
-				edge = back->edges[i];
-				// printf("back: %d -> %s - %s\n", edge->dist, back->name, edge->dest->name);
-				if (edge->dist < bsmall && !contains(connections, idx, back, edge->dest))
-					{
-						bEdge = edge;
-						bsmall = edge->dist;
-						break ;
-					}
-			}
-			if (fsmall < bsmall)
-			{
-				connections[idx++] = (struct s_connection){front, fEdge->dest};
-				print(front, fEdge);
-				front = fEdge->dest;
-			}
-			else
-			{
-				connections[idx++] = (struct s_connection){back, bEdge->dest};
-				print(back, bEdge);
-				back = bEdge->dest;
-			}
-		}
-	}
-	printf("cost: %d", cost);
-}
-
-int cycle(struct s_edge **edges, struct s_edge *edge);
-
-void printMST2(struct s_graph *graph)
-{
 	struct s_node *node, *sNode;
 	struct s_edge *edge, *sEdge;
-	int n, count, smallest, i, j;
+	int n, smallest, idx;
 
 	n = size(graph->nodes);
-	struct s_edge *sEdges[n];
-	struct s_node *sNodes[n];
-	count = 0, i = 0, j = 0;
+	int nodes[n];
+	for (int i = 0; i < n; i++)
+		nodes[i] = -1;
+	idx = 0;
 	for (int k = 1; k < n; k++)
 	{
 		smallest = INT_MAX;
@@ -161,7 +73,7 @@ void printMST2(struct s_graph *graph)
 			for (int j = 0; node->edges[j]; j++)
 			{
 				edge = node->edges[j];
-				if (edge->dist < smallest)
+				if (edge->dist < smallest && !cycle(nodes, node->id, edge->dest->id))
 				{
 					sEdge = edge;
 					smallest = edge->dist;
@@ -170,8 +82,6 @@ void printMST2(struct s_graph *graph)
 			}
 		}
 		printf("%3d : %7s - %7s\n", sEdge->dist, sNode->name, sEdge->dest->name);
-		sNodes[i++] = sNode;
-		sEdges[j++] = sEdge;
-		sEdge->dist = INT_MAX;
+		funion(nodes, find(nodes, sNode->id), find(nodes, sEdge->dest->id));
 	}
 }
